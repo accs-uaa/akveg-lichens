8b3b1ae66ae1fe1dbc1d411872e4e7d6ad94afc7
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-07-28 13:20:18 -0800

    Refactor script
    - Load functions from utils.py module rather than defining them in this script. Best practice to separate functions from main execution script
    - Style minimum_size and output_size in all caps to indicate that they are constants
    - Update list of required libraries

--- 02_resize_images.py
+++ 02_resize_images.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Resize and Copy Images
 # Author: Amanda Droghini
-# Last Updated: 2025-07-23
+# Last Updated: 2025-07-28
 # Usage: Execute in Python 3.13+.
 # Description: "Resize and Copy Images" identifies subfolders that contain images, creates short codes to use as file
 # names, resizes images not to exceed 1800px on the largest axis, and copies the resized images to the /static folder
@@ -11,9 +11,9 @@
 
 # Import required libraries
 from pathlib import Path
-import re
 import polars as pl
-from PIL import Image
+from utils import collect_img_info
+from utils import create_image_thumbnail
 
 # Set root directory
 drive = Path('C:/')
@@ -27,54 +27,13 @@ website_folder = drive / root_folder / 'Servers_Websites' / 'akveg-lichens'
 # Define output folder
 output_folder = website_folder / 'static' / 'images' / 'taxa'
 
-# Define function to check whether a folder contains one or more images
-def has_images(folder_path: Path) -> bool:
-    image_extensions = {".jpg", ".jpeg", ".png"}
-    for ext in image_extensions:
-        if next(folder_path.rglob(f"*{ext}"), None) is not None:
-            return True
-    return False
-
 # Define minimum and output image dimensions
-minimum_size = 1800
-output_size = (1800, 1800)
+MINIMUM_SIZE = 1800
+OUTPUT_SIZE = (1800, 1800)
 
-# Create empty list for storing image file paths
-img_list = []
-
-# Iterate across directories that have images
-## Exclude folders that do not start with a '_' (indicates in-progress taxa)
-## Create shorthand names and obtain image file paths
-for subfolder in taxa_folder.iterdir():
-    if subfolder.is_dir() and subfolder.name.startswith('_'):
-        if has_images(subfolder):
-            subfolder_string = str(subfolder)
-            taxon_name = subfolder.name.strip("_").strip()
-            # Create short code depending on length of string
-            short_code = re.sub(r"&\s|\.|-|(spp)|(ssp)", "", taxon_name).lower()
-            short_code = short_code.split()
-            if len(short_code)==1:
-                short_code = short_code[0]
-            elif len(short_code)==2:
-                short_code = "_".join([part[:5] for part in short_code])
-            elif len(short_code)>2 and short_code[0] == short_code[2]:
-                del (short_code[2])
-                short_code = "_".join([part[:5] for part in short_code])
-            else:
-                short_code = "_".join([part[:5] for part in short_code])
-
-            # Obtain image path and append to image list
-            image_extensions = {".jpg", ".jpeg", ".png"}
-            for ext in image_extensions:
-                for image_path in subfolder.rglob(f"*{ext}"):
-                    img_string = str(image_path)
-                    img_list.append({"folder_path": subfolder_string,
-                                 "taxon_name": taxon_name,
-                                 "short_code": short_code,
-                                 "input_path": img_string,
-                                 "image_ext": image_path.suffix})
-        else:
-            print(f"'{subfolder.name}': No images found.")
+# List all image files in subdirectories of taxa_folder
+## Append subfolder path, taxon name, and taxonomic short code
+img_list = collect_img_info(taxa_folder)
 
 # Convert to polars df
 img_files = pl.DataFrame(img_list)
@@ -113,11 +72,11 @@ img_files = img_files.with_columns(
     .alias("output_path"))
 
 # Resize and copy images
-for new_path in img_files['output_path']:
-    src_path = Path(img_files.filter(pl.col('output_path') == new_path)['input_path'].item())
-    with Image.open(src_path) as im:
-        if (im.width < minimum_size) and (im.height < minimum_size):
-            print(f"{src_path} was not resized; dimension too small.")
-        else:
-            im.thumbnail(output_size)
-            im.save(new_path)
+print("Starting image processing loop...")
+for row in img_files.iter_rows(named=True):
+    input_path = row["input_path"]
+    output_path = row["output_path"]
+
+    # Call the function for each image pair
+    create_image_thumbnail(input_path, output_path, MINIMUM_SIZE, OUTPUT_SIZE)
+print("Image processing loop finished.")
