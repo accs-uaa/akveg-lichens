6f3b17b4554bf74ba8cee6daaf5dda791625a410
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-07-28 16:27:34 -0800

    Add export as CSV
    To use in 03_format_docx.py script
    
    Change name of objects from 'taxa_org' and 'org_folders' to 'hierarchy' and 'hierarchy_folders'

--- 01_create_toc.py
+++ 01_create_toc.py
@@ -2,11 +2,12 @@
 # ---------------------------------------------------------------------------
 # Create Table of Contents
 # Author: Amanda Droghini
-# Last Updated: 2025-07-22
+# Last Updated: 2025-07-28
 # Usage: Execute in Python 3.13+.
-# Description: "Create Table of Contents" creates a website folder and index file for each taxonomic group. The
-# script formats group names and adds front matter to the index Markdown file for compatibility with the static website
-# generator Hugo.
+# Description: "Create Table of Contents" creates a folder and Markdown file for each taxonomic group. The
+# script formats taxonomic group names, creates folders and Markdown files, and adds front matter to the Markdown
+# files for compatibility with Hugo. The script also exports a dataframe that links the taxon's original folder name
+# with its processed folder name for use in subsequent scripts.
 # ---------------------------------------------------------------------------
 
 # Import required libraries
@@ -19,18 +20,28 @@ drive = Path('C:/')
 root_folder = 'ACCS_Work'
 
 # Define folder structure
-project_folder = drive / root_folder / 'Projects' / 'Lichen_Guide' / 'Guide Master Folder_V_7_16_25'
+project_folder = drive / root_folder / 'Projects' / 'Lichen_Guide'
 website_folder = drive / root_folder / 'Servers_Websites' / 'akveg-lichens'
+output_folder = project_folder / 'outputs'
 
-# Define input files
-taxa_org_input = project_folder / 'Guide_Master_Reference_7_13_25.xlsx'
+# Define input file
+hierarchy_input = project_folder / 'Guide Master Folder_V_7_16_25' / 'Guide_Master_Reference_7_13_25.xlsx'
+
+# Define output file
+output_csv = output_folder / 'taxon_hierarchy.csv'
 
 # Read input file
-taxa_org = pl.read_excel(taxa_org_input, columns=['Organization (variable)',
+hierarchy = pl.read_excel(hierarchy_input, columns=['Organization (variable)',
                                                   'Taxa Folder Name'])
 
+# Remove whitespaces from folder names
+hierarchy = hierarchy.with_columns(
+    pl.col("Taxa Folder Name")
+    .str.strip_chars()
+)
+
 # Correct group for Squamarina lentigera
-taxa_org = taxa_org.with_columns(
+hierarchy = hierarchy.with_columns(
     pl.when(pl.col("Taxa Folder Name") == "Squamarina lentigera")
     .then(pl.lit("Lobed Crusts, Select Crusts, Minutely Fruticose Species"))
     .otherwise(pl.col('Organization (variable)'))
@@ -46,13 +57,13 @@ mapping = {"gray crustose Caliciaceae": "Caliciaceae",  ## Combine into single C
                                                                       "Minutely Fruticose Species",
            " spp.": ""}
 
-taxa_org = taxa_org.with_columns(
+hierarchy = hierarchy.with_columns(
     pl.col('title_name')
     .str.replace_many(mapping)
     .alias('title_name'))
 
 # Create folder names
-## Further simplify of verbose groups
+## Further simplify verbose groups
 mapping = {"Mushroom-Forming Lichens & Basidiolichens": "Mushroom-Forming",
            "Lobed Crusts, Select Crusts, Minutely Fruticose Species": "Crusts & Fruticose",
            "Pseudocyphellaria & Parmostictina": "Pseudo & Parmo",
@@ -60,34 +71,36 @@ mapping = {"Mushroom-Forming Lichens & Basidiolichens": "Mushroom-Forming",
            "P. aphthosa-leucophlebia Complex & Similar": "Pelaph Complex",
            " Genera": ""}
 
-taxa_org = taxa_org.with_columns(
+hierarchy = hierarchy.with_columns(
     pl.col('title_name')
     .str.replace_many(mapping)
     .str.replace_all(" Lichens", "")
     .str.to_lowercase()
-    # .str.replace_many(["likes"], ["like"])
     .str.replace_all(r"\s|&|-", "_")
     .str.replace_all(r"_+", value="_")
     .alias('folder_name'))
 
+# Remove duplicates
+hierarchy = hierarchy.unique(subset=['Taxa Folder Name', "title_name", "folder_name"])
+
 # Extract unique folder names
-org_folders = (taxa_org
+hierarchy_folders = (hierarchy
                .unique(subset=['title_name', "folder_name"])
                .select(['title_name', "folder_name"]))
 
 # Determine order of table of contents
 ## Start at 10 to avoid overlap with intro pages
 ## Random ordering for now - Need Preston's feedback
-org_folders = org_folders.with_columns(
+hierarchy_folders = hierarchy_folders.with_columns(
     (pl.int_range(10, pl.len() + 10)).alias("order")
 )
 
 # Create new folders and index files
-for new_folder in org_folders['folder_name']:
+for new_folder in hierarchy['folder_name']:
     folder_path = Path(website_folder / 'content' / 'pages' / 'taxa' / new_folder)
     index_path = Path(folder_path / '_index.md')
-    index_title = org_folders.filter(pl.col('folder_name') == new_folder)['title_name'].item()
-    index_weight = org_folders.filter(pl.col('folder_name') == new_folder)['order'].item()
+    index_title = hierarchy.filter(pl.col('folder_name') == new_folder)['title_name'].item()
+    index_weight = hierarchy.filter(pl.col('folder_name') == new_folder)['order'].item()
     Path.mkdir(folder_path, parents=True, exist_ok=True)
 
     with (open(index_path, 'w', encoding='utf-8') as index_file):
@@ -105,3 +118,6 @@ for new_folder in org_folders['folder_name']:
         """)
 
         index_file.write(front_matter)
+
+# Export hierarchy df as CSV
+hierarchy.write_csv(output_csv)
