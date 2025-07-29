d394e1842b11233030b652b8fff2dfd8a66a6b58
Amanda Droghini <10262153+adroghini@users.noreply.github.com>
2025-07-29 15:01:15 -0800

    Use generate_taxon_name
    Ensures taxon_name matches taxon_name output from other scripts
    
    Rename columns for readability
    
    Make use of iter_rows instead of standard for loop, as this simplifies extracting row values

--- 01_create_toc.py
+++ 01_create_toc.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------------
 # Create Table of Contents
 # Author: Amanda Droghini
-# Last Updated: 2025-07-28
+# Last Updated: 2025-07-29
 # Usage: Execute in Python 3.13+.
 # Description: "Create Table of Contents" creates a folder and Markdown file for each taxonomic group. The
 # script formats taxonomic group names, creates folders and Markdown files, and adds front matter to the Markdown
@@ -14,6 +14,7 @@
 from pathlib import Path
 import polars as pl
 import textwrap
+from utils import generate_taxon_name
 
 # Set root directory
 drive = Path('C:/')
@@ -31,20 +32,26 @@ hierarchy_input = project_folder / 'Guide Master Folder_V_7_16_25' / 'Guide_Mast
 output_csv = output_folder / 'taxon_hierarchy.csv'
 
 # Read input file
-hierarchy = pl.read_excel(hierarchy_input, columns=['Organization (variable)',
+hierarchy = (pl.read_excel(hierarchy_input, columns=['Organization (variable)',
                                                   'Taxa Folder Name'])
+             .rename({'Organization (variable)': "taxon_grouping",
+         "Taxa Folder Name": "original_folder"
+         }))
 
-# Remove whitespaces from folder names
+# Format taxon folder name
+## Strips whitespaces and enforces consistent spelling of taxonomic abbreviations
 hierarchy = hierarchy.with_columns(
-    pl.col("Taxa Folder Name")
-    .str.strip_chars()
+    pl.col("original_folder").map_elements(generate_taxon_name,
+                                           return_dtype=pl.String)
+    .alias(
+        "original_folder")
 )
 
-# Correct group for Squamarina lentigera
+# Correct grouping for Squamarina lentigera
 hierarchy = hierarchy.with_columns(
-    pl.when(pl.col("Taxa Folder Name") == "Squamarina lentigera")
+    pl.when(pl.col("original_folder") == "Squamarina lentigera")
     .then(pl.lit("Lobed Crusts, Select Crusts, Minutely Fruticose Species"))
-    .otherwise(pl.col('Organization (variable)'))
+    .otherwise(pl.col('taxon_grouping'))
     .alias('title_name')
 )
 
@@ -54,8 +61,7 @@ mapping = {"gray crustose Caliciaceae": "Caliciaceae",  ## Combine into single C
            "[Non-Shrub or Hair-Like] Fruticose Parmelioids": "Non-Shrub or Hair-Like Fruticose Parmelioids",
            "P. aphthosa-leucophlebia complex & similar": "P. aphthosa-leucophlebia Complex & Similar",
            "Lobed-Crusts, Select Crusts, Minutely Fruticose Species": "Lobed Crusts, Select Crusts, "
-                                                                      "Minutely Fruticose Species",
-           " spp.": ""}
+                                                                      "Minutely Fruticose Species",}
 
 hierarchy = hierarchy.with_columns(
     pl.col('title_name')
@@ -68,25 +74,25 @@ mapping = {"Mushroom-Forming Lichens & Basidiolichens": "Mushroom-Forming",
            "Lobed Crusts, Select Crusts, Minutely Fruticose Species": "Crusts & Fruticose",
            "Pseudocyphellaria & Parmostictina": "Pseudo & Parmo",
            "Non-Shrub or Hair-Like Fruticose Parmelioids": "Non-Shrub Hair",
-           "P. aphthosa-leucophlebia Complex & Similar": "Pelaph Complex",
-           " Genera": ""}
+           "P. aphthosa-leucophlebia Complex & Similar": "Pelaph Complex",}
 
 hierarchy = hierarchy.with_columns(
     pl.col('title_name')
     .str.replace_many(mapping)
-    .str.replace_all(" Lichens", "")
+    .str.replace_all(" Lichens|\\.", "")
     .str.to_lowercase()
     .str.replace_all(r"\s|&|-", "_")
     .str.replace_all(r"_+", value="_")
-    .alias('folder_name'))
+    .alias('taxon_folder'))
 
 # Remove duplicates
-hierarchy = hierarchy.unique(subset=['Taxa Folder Name', "title_name", "folder_name"])
+hierarchy = (hierarchy.unique(subset=['original_folder', "title_name", "taxon_folder"])
+                             .select(['original_folder', "title_name", "taxon_folder"]))
 
 # Extract unique folder names
 hierarchy_folders = (hierarchy
-               .unique(subset=['title_name', "folder_name"])
-               .select(['title_name', "folder_name"]))
+               .unique(subset=['title_name', "taxon_folder"])
+               .select(['title_name', "taxon_folder"]))
 
 # Determine order of table of contents
 ## Start at 10 to avoid overlap with intro pages
@@ -96,11 +102,12 @@ hierarchy_folders = hierarchy_folders.with_columns(
 )
 
 # Create new folders and index files
-for new_folder in hierarchy['folder_name']:
-    folder_path = Path(website_folder / 'content' / 'pages' / 'taxa' / new_folder)
+for row in hierarchy_folders.iter_rows(named=True):
+    taxon_folder = row['taxon_folder']
+    folder_path = Path(website_folder / 'content' / 'pages' / 'taxa' / taxon_folder)
     index_path = Path(folder_path / '_index.md')
-    index_title = hierarchy.filter(pl.col('folder_name') == new_folder)['title_name'].item()
-    index_weight = hierarchy.filter(pl.col('folder_name') == new_folder)['order'].item()
+    index_title = row['title_name']
+    index_weight = row['order']
     Path.mkdir(folder_path, parents=True, exist_ok=True)
 
     with (open(index_path, 'w', encoding='utf-8') as index_file):
